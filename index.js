d33da729ec2870efce917a8b3cd041ff32ed91bf
Raja Akash Kumar <rajaakashk9@gmail.com>
2024-10-14 15:02:01 +0530

    Updated index.js with latest changes

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use(express.json());
 intializeDatabase();
 
 const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors());
 
 const newHotel1 = {
